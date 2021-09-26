ca53297828d0d57a3534896b406a10690155d80c
Matthew Sievers <mattersievers@gmail.com>
2021-09-25 21:29:15 -0500

    dashboard complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const { response } = require('express');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req,res) => {
     console.log('====================');
@@ -76,11 +77,11 @@ router.get('/:id', (req,res) => {
     });
 });
 
-router.post('/', (req,res) => {
+router.post('/', withAuth, (req,res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch( err => {
@@ -89,7 +90,7 @@ router.post('/', (req,res) => {
         });
 });
 
-router.put('/upvote', (req,res) => {
+router.put('/upvote', withAuth, (req,res) => {
     // make sure the session exists first
     if(req.session) {
         //pass session id along with all the destructured properties on req.body
@@ -102,7 +103,7 @@ router.put('/upvote', (req,res) => {
     }
 });
 
-router.put('/:id', (req,res) => {
+router.put('/:id', withAuth, (req,res) => {
     Post.update(
         {
             title: req.body.title
@@ -126,7 +127,7 @@ router.put('/:id', (req,res) => {
         });
 });
 
-router.delete('/:id', (req,res) => {
+router.delete('/:id', withAuth, (req,res) => {
     Post.destroy({
         where: {
             id: req.params.id
